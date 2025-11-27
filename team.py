46db12c1a8dc3e0fe53e950a9bf6c67eacb745c7
muchenyukibana-dev <muchen.yukibana@gmail.com>
2025-11-27 11:47:28 -0600

    Update team.py

--- team.py
+++ team.py
@@ -67,7 +67,7 @@ st.markdown("""
         justify-content: center;
         width: 100%;
         /* 🔥 右移 40px */
-        margin-left: 40px; 
+        margin-left: 400px; 
     }
     .stButton>button {
         background-color: #FF0055;
@@ -388,3 +388,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
